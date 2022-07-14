8a0b1d872a17b85b6e2ea016a1c45a572570dfb8
Muhammad Rifandy Syahermi <107825688+Rifandy17@users.noreply.github.com>
2022-07-14 12:26:30 +0700

    Array-Buah Buahan
    Array-Buah Buahan

---
+++ Membuat array buah-buahan.py
@@ -0,0 +1,8 @@
+# Membuat array buah-buahan
+# Jumlah Index 6
+# Looping menggunakan for
+
+buah = ['mangga', 'apel', 'anggur', 'jeruk', 'pisang', 'nanas', 'ceri']
+
+for  buah_buahan in buah:
+    print("Nama buah : ", buah_buahan)
